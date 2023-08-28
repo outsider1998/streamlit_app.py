8a049f4aa2689d6e95085c5e57ccffd45424d9c2
outsider1998 <143381539+outsider1998@users.noreply.github.com>
2023-08-29 00:03:11 +0530

    Update untitled8.py

--- untitled8.py
+++ untitled8.py
@@ -7,10 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1O0XypimjbNroPoP2LFRD4gVch6QFKWgr
 """
 
-pip install streamlit
-
-import streamlit as st
-
 def find_largest_number(a, b, c):
     return max(a, b, c)
 
